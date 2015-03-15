98194fdf4e1aab9625a14a5fb1b4cbc585e7b24e
biz-manager <biz-manager@users.noreply.github.com>
2015-03-15 16:46:06 +0200

    Create Light_01.js

---
+++ Light_01.js
@@ -0,0 +1,42 @@
+var Light = function (color) {
+    // constructor
+   
+       this.color = color;
+       this.is_on = false;
+
+}
+
+Light.prototype = {
+    
+    constructor: Light,
+    
+    states: [ 'off', 'on' ],
+    
+    turnOn: function() {
+      
+        this.switch(true);
+    },
+    
+    turnOff: function() {
+       
+        this.switch(false);
+    },
+    
+    switch: function (is_on) {
+     
+        if(this.is_on == ! is_on) {
+            
+          this.is_on = is_on;
+         /* console.info(this.color + ' has been turned ' + this.states[ new Number(is_on) ]); */
+          return this.is_on;
+        }
+       /* console.warn(this.color + ' is already ' + this.states[ new Number(this.is_on) ]); */
+        return this.is_on;
+        },
+    
+       isOn: function () {
+           return this.is_on;
+       }
+   
+    
+}
